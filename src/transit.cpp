557c8257dda9fdfb9828f5bc96fdcf49992b69ff
Yurii <yura-mel03@mail.ru>
2026-07-30 00:06:53 +0300

    Разделение методов на файлы, удаление лишних файлов

--- src/transit.cpp
+++ src/transit.cpp
@@ -5,29 +5,55 @@
 using namespace std;
 
 
-string func_DFA::transit(const vector<string>& input, string current,
-              const std::unordered_map<string, std::unordered_map<string, string>>& transitions) {
+namespace func_DFA{
+
+    
+void DFA5::transit_string(const vector<string>& str) {
+    cout << "\n===== Осуществление перехода ====="<< endl;
+    cout << "Переход из состояния - " << current_state << endl;
+    cout << "По строке - "; 
+    auto flag = true;
     // Обработка каждого символа
-    for (string ch : input) {   
+    for (string ch : str) {   
+        // Вывод строки перехода
+        if (flag) {
+            cout << ch;
+            flag = false;
+        }
+        else{
+            cout << ", " << ch;
+        }
+        
         // Ищем таблицу переходов из текущего состояния
-        auto state_it = transitions.find(current);
+        auto state_it = transitions.find(current_state);
         if (state_it == transitions.end()) {
-            std::cout << "Нет переходов из состояния " << current << "\n";
+            std::cout << "\n-----------Нет переходов из состояния " << current_state << "---------------\n";
             break; // автомат застрял
         }
         // Ищем переход по текущему символу
         // Внутренняя таблица – это unordered_map<string, string>
         auto sym_it = state_it->second.find(ch);
         if (sym_it == state_it->second.end()) {
-            std::cout << "Нет перехода по символу '" << ch << "'\n";
+            std::cout << "\n------------Нет перехода по символу '" << ch << "'-------------------\n";
             break; // переход не определён
         }
 
         // Выполняем переход
-        current = sym_it->second;
-        //std::cout << "Переход по '" << ch << "' → состояние " << current << "\n";
+        current_state = sym_it->second;
+        //std::cout << "\nПереход по '" << ch << "' → состояние " << current_state << "\n";
     }
-    //std::cout << "\n";
-    return current;
+    cout << "\nВ состояние - " << current_state << endl;
+    cout << endl;
+
 }
 
+
+void DFA5::transit_Input(const vector<string>& input){
+    transit_string(input);
+}
+
+void DFA5::transit_fromCSV(){
+    transit_string(string_transition);
+}
+
+}
